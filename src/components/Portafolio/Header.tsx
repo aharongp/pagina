c9798fe0c003404b569dc77d20edf6cb2b01bbcf
aharonguedez <aharon.guedez23@gmail.com>
2025-03-30 10:30:27 -0400

    add meteors

--- src/components/Portafolio/Header.tsx
+++ src/components/Portafolio/Header.tsx
@@ -7,11 +7,6 @@ export default function Header() {
       <div className="flex justify-center py-8">
         <img src={logo} alt="Logo BAI automation" width={305} className="animate-pulse"/>
       </div>
-      <a href="https://cal.com/baiautomation/60-min-meeting">
-        <button className="bg-[#b89595] text-gray-100 mt-5 px-6 py-3 rounded-lg hover:bg-[#4b1515] transition shadow-lg shadow-[#d4a5a5]/100">
-          Agendar consultoría <span className="font-bold">GRATIS</span>
-        </button>
-      </a>
       <Link to="/" className="text-black hover:text-[#b89595] flex items-center mb-4 mt-6">
           <ArrowLeft className="w-4 h-4 mr-2" />
           Volver
